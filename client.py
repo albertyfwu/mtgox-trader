591dbb1137f220d5a522b500dfa08c4e5cff04ca
albertyfwu <albertyfwu@gmail.com>
2013-05-29 21:22:25 -0400

    preliminary mtgox requests communication

--- client.py
+++ client.py
@@ -1,3 +1,35 @@
-# Testing
+import hmac, base64, hashlib, urllib, requests
+import time
 
-print 'init'
+base = 'https://data.mtgox.com/api/2/'
+
+class Client:
+
+    def __init__(self, key, secret):
+        self.key = key
+        self.secret = base64.b64decode(secret)
+
+    def _nonce(self):
+        return str(int(time.time() * 1e6))
+
+    def _request(self, path, params={}):
+        params['nonce'] = self._nonce()
+        data = urllib.urlencode(params)
+        hash_data = path + chr(0) + data
+        sha512 = hashlib.sha512
+        sign = base64.b64encode(str(hmac.new(self.secret, hash_data, sha512).digest()))
+
+        headers = {
+            'User-Agent': 'mtgox-trader',
+            'Rest-Key': self.key,
+            'Rest-Sign': sign
+        }
+
+        try:
+            request = requests.post(base + path, data=params, headers=headers)
+            return request.text
+        except Exception as e:
+            print e
+
+    def doStuff(self):
+        print self._request('BTCUSD/money/ticker')
---
+++ main.py
@@ -0,0 +1,10 @@
+import json
+
+from client import Client
+
+api = json.load(open('./api_info.json'))
+MTGOX_KEY = api['MTGOX_KEY']
+MTGOX_SECRET = api['MTGOX_SECRET']
+
+client = Client(MTGOX_KEY, MTGOX_SECRET)
+client.doStuff()
